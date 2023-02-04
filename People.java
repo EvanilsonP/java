7e209af02fadd5946f781a89608e38a72df08a53
EvanilsonP <pevanilson99@gmail.com>
2023-02-04 20:07:32 -0300

    Function example

---
+++ People.java
@@ -0,0 +1,13 @@
+public class People {
+    public static void printName(String name) {
+        System.out.println("Hey! My name is " + name + "!");
+    }
+    public static void main(String[] args) {
+
+        String name1 = "Mary";
+        printName(name1);
+
+        String name2 = "Jully";
+        printName(name2);
+    }
+};
