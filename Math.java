c3067be9127356d0f1daf9fc9fd7f383243c1b62
EvanilsonP <pevanilson99@gmail.com>
2023-02-06 21:36:24 -0300

    Math with values

---
+++ Math.java
@@ -0,0 +1,32 @@
+public class Math {
+    public static void main(String[] args) {
+
+        int a = 3 + 3;
+        int b = 10 - 5;
+        int c = 20 * 5;
+        int d = 25 / 5;
+        int e = 17 % 3 ;
+        int answer = 2 + 3 * 4;
+        int answer2 = ( 2 + 3 ) * 4;
+        int g = 17 % 3 + 7;
+        
+        int base = 10;
+        int height = 15;
+        double area = base / 2 * height;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(answer);
+        System.out.println(answer2);
+        System.out.println(g);
+        System.out.println(area);
+
+    }
+
+    private static int abs(int i) {
+        return 0;
+    }
+};
